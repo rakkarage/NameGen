3ab33a1bd9ee1a2214e680ef068ebd4fa8a543c3
rakkarage <rakkarage@gmail.com>
2011-08-28 03:45:58 -0300

    Edited NameGenerator.h via GitHub

--- NameGenerator.h
+++ NameGenerator.h
@@ -1,25 +1,23 @@
 @interface NameGenerator : NSObject
 {
-
+  NSMutableArray *vowel;
+	
+	NSMutableArray *malePre;
+	NSMutableArray *maleStart;
+	NSMutableArray *maleMiddle;
+	NSMutableArray *maleEnd;
+	NSMutableArray *malePost;
+	
+	NSMutableArray *male;
+	
+	NSMutableArray *femalePre;
+	NSMutableArray *femaleStart;
+	NSMutableArray *femaleMiddle;
+	NSMutableArray *femaleEnd;
+	NSMutableArray *femalePost;
+	
+	NSMutableArray *female;
 }
-
-@property(nonatomic, retain) NSMutableArray *vowel;
-
-@property(nonatomic, retain) NSMutableArray *malePre;
-@property(nonatomic, retain) NSMutableArray *maleStart;
-@property(nonatomic, retain) NSMutableArray *maleMiddle;
-@property(nonatomic, retain) NSMutableArray *maleEnd;
-@property(nonatomic, retain) NSMutableArray *malePost;
-
-@property(nonatomic, retain) NSMutableArray *male;
-
-@property(nonatomic, retain) NSMutableArray *femalePre;
-@property(nonatomic, retain) NSMutableArray *femaleStart;
-@property(nonatomic, retain) NSMutableArray *femaleMiddle;
-@property(nonatomic, retain) NSMutableArray *femaleEnd;
-@property(nonatomic, retain) NSMutableArray *femalePost;
-
-@property(nonatomic, retain) NSMutableArray *female;
 
 - (NSString *)getName;
 
